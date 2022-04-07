c039dcd0b2092e3f63faa13ea5cb42e5ca4ebef2
Mark Clu Heramis <chumheramis@gmail.com>
2022-04-07 09:40:00 +0800

    usable class create a usable class with callback functions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # playground-node-create-directory
 Created with CodeSandbox
+
+Play around with creation of directories
---
+++ src/directory_creator.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+/**
+ *
+ * @param String namespace This variable should contain the value passed from the Gitlab Webhook Event Data `event_data.object_attributes.source.path_with_namespace`
+ * @param String branch This variable should contain the value passed from the Gitlab Webhook Event Data `event_data.object_attributes.source_branch
+ * @param function success_callback
+ * @param function error_callback
+ */
+export default function (
+  namespace,
+  branch,
+  success_callback = undefined,
+  error_callback = undefined
+)  {
+  namespace = namespace.replace("/", "_");
+  let directory_name = namespace + "_" + branch;
+
+  fs.mkdir(path.join(__dirname, directory_name), (fs_error) => {
+    if (fs_error && error_callback !== undefined) {
+      error_callback(fs_error);
+      return;
+    }
+    if (success_callback !== undefined) {
+      success_callback();
+    }
+  });
+};
